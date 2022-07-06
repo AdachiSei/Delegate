11529c413296fe1e9fb67e2ce80437c90e5c6993
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-07-06 16:18:44 +0900

    最初のコミット

---
+++ Assets/Scripts/System.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class System : MonoBehaviour
+{
+    void Start()
+    {
+        Timer timer = new Timer();
+        timer.onComplete.AddListener(Alarm);
+        timer.CountDown();
+    }
+
+    void Alarm()
+    {
+        Debug.Log("カウントダウン終了");
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events; // 必要！
+
+public class Timer
+{
+    public UnityEvent onComplete = new UnityEvent();
+
+    public void CountDown()
+    {
+        // 本当はちゃんと3秒かけてカウントする
+        Debug.Log(3);
+        Debug.Log(2);
+        Debug.Log(1);
+
+        // コールバック
+        onComplete.Invoke();
+    }
+}
